c29604a95cb6c2d7034df7538965eb33b38adc25
Juanfi-Arias <juan13arias3jimenez2005@gmail.com>
2020-12-14 19:34:19 -0300

    contador de clicks para pacho

---
+++ Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Contador_de_cliks
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "You win 👏🏆")
+            {
+                textBox1.Text = "0";
+                return;
+            }
+            int count = int.Parse(textBox1.Text);
+            count++;
+            textBox1.Text = count.ToString();
+
+            if (textBox1.Text == "101")
+                textBox1.Text = "You win 👏🏆";
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text="0";
+        }
+    }
+}
